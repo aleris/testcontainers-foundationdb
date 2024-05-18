ef5cf190f87e9b6656dce5bdeeeace041155c231
Adrian Tosca <atosca@adobe.com>
2024-05-18 12:35:50 +0300

    fix readme version update script

--- scripts/updateReadmeVersion.sh
+++ scripts/updateReadmeVersion.sh
@@ -2,9 +2,7 @@
 
 # Replace the version in README.md with the version from build.gradle.kts
 # Usage:
-#   ./scripts/updateReadmeVersion.sh <module>
-# Example:
-#   ./scripts/updateReadmeVersion.sh earth.adi
+#   ./scripts/updateReadmeVersion.sh
 
 SED="sed"
 if ! command -v gsed &> /dev/null
@@ -13,9 +11,8 @@ then
   SED="gsed"
 fi
 
-VERSION=$(SED -n 's/^version\s*=\s*"\([0-9]\.[0-9]\.[0-9]\)"/\1/p' < build.gradle.kts)
+VERSION=$(SED -n "s/^version\s*'\([0-9]\.[0-9]\.[0-9]\)'/\1/p" < build.gradle)
 
 echo "Updating to version $VERSION in README.md..."
-SED -i -E 's/earth\.adi:.+:[0-9]+\.[0-9]+\.[0-9]+/earth\.adi:.+:'"${VERSION}"'/' README.md
-SED -i -E 's|<version>[0-9]+\.[0-9]+\.[0-9]+|<version>'"${VERSION}"'|' README.md
-SED -i -E 's/Version-[0-9]+\.[0-9]+\.[0-9]+-blue/Version-'"${VERSION}"'-blue/' README.md
+SED -i -E 's/earth.adi:testcontainers-foundationdb:[0-9]+\.[0-9]+\.[0-9]+/earth.adi:testcontainers-foundationdb:'"${VERSION}"'/' README.md
+sed -i -E 's|testcontainers-foundationdb</artifactId>\n    <version>[0-9]+\.[0-9]+\.[0-9]+|testcontainers-foundationdb</artifactId>\n    <version>'"${VERSION}"'|' README.md
